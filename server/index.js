45b0283ee6978bda998290905b7a15826182b886
cs6662128 <s6604062662128@email.kmutnb.ac.th>
2026-02-21 18:15:56 +0700

    add user authentication (register & login)

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+
+const initMySQL = require("./src/config/db");
+const authRoutes = require("./src/routes/auth.routes");
+// const productRoutes = require("./src/routes/product.routes");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:8000"],
+  }),
+);
+
+const port = process.env.PORT || 8000
+
+let conn;
+
+app.use((req, res, next) => {
+  req.db = conn;
+  next();
+});
+
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/product", productRoutes);
+
+app.listen(port, async (req,res) => {
+  conn =await initMySQL() // เรียกจาก db.js
+  console.log(`http server run at : http://localhost:${port} `)
+})
---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+
+exports.register = async (req, res) => {
+
+    let user = req.body
+    try{
+        const passwordHash = await bcrypt.hash(user.password, 10)
+        console.log('hash: ', passwordHash)
+        user.password = passwordHash
+
+        const idcardHash = await bcrypt.hash(user.idcard, 10)
+        user.idcard = idcardHash
+        await req.db.query("INSERT INTO user SET ?", user)
+        res.status(201).json({
+            message: "Register successful",
+        });
+    }catch(error){
+        console.log('error: ', error)
+        res.status(400).json({
+        message: "Register failed",
+        error
+    });
+    }
+
+};
+
+
+exports.login = async (req, res) =>{
+    const{email, password} = req.body
+    
+    try{
+        const result = await req.db.query("SELECT * FROM user WHERE email = ?", email)
+        if(result[0].length > 0){
+            console.log("email was found")
+            const oldPassword = result[0][0].password
+            const match = await bcrypt.compare(password, oldPassword)
+            if(!match){
+                return res.status(400).json({
+                    message: "Password is not correct!"
+                });
+            }
+
+            const token =  jwt.sign({email, role: "user"},  process.env.JWT_SECRET, { expiresIn: '1h'})
+
+            res.json({
+                message : "Login succesful!",
+                token
+            })
+        }else{
+            return res.status(401).json({
+                message: "Email not found",
+            });
+        }
+    }catch(error){
+        console.log("Error message: ", error)
+        res.status(500).json({
+            message: "Login failed",
+            error: error.message,
+        });
+    }
+};
+
+
