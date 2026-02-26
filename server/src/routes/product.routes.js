64cb0223ff273dfcd1fc58bf10b0e2397ba1c85b
Apiwat-Tassananutriyaporn <apiwat150448@gmail.com>
2026-02-27 00:23:55 +0700

    modify comment (productRoutes)

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -4,23 +4,20 @@ const productController = require("../controllers/product.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 
-router.patch("/register",verifyToken, authorizeRoles("admin"), productController.registerProductToUser);
+router.patch("/register",verifyToken, authorizeRoles("admin"), productController.registerProductToUser);//register product 
 router.post("/add",verifyToken, authorizeRoles("admin"), productController.addProduct); //add product
-router.get("/get",verifyToken, authorizeRoles("admin"), productController.getAllProducts);  // get all product
+router.get("/get",verifyToken, authorizeRoles("admin"), productController.getAllProducts);  // get all products
 
-router.get("/collection",verifyToken, authorizeRoles("user"), productController.getMyCollection); //get my product 0
-router.patch("/:id/sellcollection",verifyToken, authorizeRoles("user"), productController.sellCollection); //sell product 1
+router.get("/collection",verifyToken, authorizeRoles("user"), productController.getMyCollection); //get my product 
+router.patch("/:id/sellcollection",verifyToken, authorizeRoles("user"), productController.sellCollection); //sell product 
 router.get("/market", productController.market);// get all products for sale 2
-router.patch("/:id/buycollection", verifyToken, authorizeRoles("user"), productController.buyCollection); // buy product 3
+router.patch("/:id/buycollection", verifyToken, authorizeRoles("user"), productController.buyCollection); // buy product 
 
-router.get("/asset", verifyToken, authorizeRoles("user"), productController.getMyAsset); //get my asset 0 
-router.patch("/:id/sellasset", verifyToken, authorizeRoles("user"), productController.sellAsset); // sell asset 1
-router.get("/trading", productController.trading); // get all assets for sale  2
-router.patch("/:id/buyasset", verifyToken, authorizeRoles("user"), productController.buyAsset);//buy asset 3
+router.get("/asset", verifyToken, authorizeRoles("user"), productController.getMyAsset); //get my asset  
+router.patch("/:id/sellasset", verifyToken, authorizeRoles("user"), productController.sellAsset); // sell asset 
+router.get("/trading", productController.trading); // get all assets for sale  
+router.patch("/:id/buyasset", verifyToken, authorizeRoles("user"), productController.buyAsset);//buy asset 
 
-router.get("/:serial/verify", productController.verify);//buy asset 3//verify product
-
-
-
+router.get("/:serial/verify", productController.verify);//verify product
 
 module.exports = router;
