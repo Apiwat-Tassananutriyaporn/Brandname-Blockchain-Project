af61db434d7a270562e5dc05d36fcc03a6639709
Apiwat-Tassananutriyaporn <apiwat150448@gmail.com>
2026-03-26 21:21:56 +0700

    update mycollection page

--- app/Allpage/MyCollection/page.tsx
+++ app/Allpage/MyCollection/page.tsx
@@ -189,7 +189,7 @@ export default function CollectionPage() {
                 </div>
 
                 <div className="flex items-center gap-6 w-full md:w-auto justify-between md:justify-end border-t md:border-t-0 pt-4 md:pt-0 mt-2 md:mt-0">
-                  <p className="text-xl font-bold text-black">{Number(item.price).toLocaleString()} ETH</p>
+                  <p className="text-xl font-bold text-black">{Number(item.price).toLocaleString()}ETH</p>
                   <button 
                     onClick={() => {
                       setSelectedProduct(item);
