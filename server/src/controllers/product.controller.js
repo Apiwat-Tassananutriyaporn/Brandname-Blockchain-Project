c615a3f4a2698475f1dcb89e1a1c7573e1b8c4e5
Apiwat-Tassananutriyaporn <apiwat150448@gmail.com>
2026-03-26 17:03:44 +0700

    update product controller&routes

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -28,12 +28,36 @@ exports.registerProductToUser = async (req, res) => {
     }
 };
 
+exports.getWalletAddress = async (req, res) => {
+    const {email} = req.body
+     try{
+        const [user] = await req.db.query("SELECT wallet_address FROM user WHERE email = ?", email)
+
+        if (user.length === 0 || !user[0].wallet_address) {
+            return res.status(404).json({ message: "ไม่พบ Wallet Address ของอีเมลนี้" });
+        }
+
+        res.json({
+            message: "SELECT complete!",
+            data: user[0].wallet_address
+        })
+
+    }catch(error){
+        console.log("can not get walltet address")
+        res.status(500).json({
+            message: "something wrong",
+            error
+        })
+    }
+};
+
 exports.addProduct = async (req, res) => {
     const product = req.body
-
+    console.log("product: ", product)
     try{
         let id
-        let brand = product.Brand
+        let brand = product.brandName
+        const user_token = req.user
         
         switch(brand) {
         case "Chanel":
@@ -50,8 +74,19 @@ exports.addProduct = async (req, res) => {
             break;
         }
         product.brand_id = id
-        delete product.Brand
-        const result = await req.db.query("INSERT INTO product SET ?", product)
+        product.current_owner_id = user_token.id
+        delete product.brandName
+        if(product.blockchain_status === "Minted"){
+            delete product.blockchain_status
+            delete product.wallet_address
+            if(product.type === "Real"){
+                product.extra =  "NULL"
+                product.change = "NULL"
+                product.isPositive =  "NULL"
+            }
+            console.log("Again product: ", product)
+            const result = await req.db.query("INSERT INTO product SET ?", product)
+        }
         res.json({
             message: "Add Product succesful!!",
             product
@@ -85,23 +120,39 @@ exports.getAllProducts = async (req, res) => {
 
 };
 
+exports.OwnerShipHistory = async (req, res) => {
+    const id = req.params.id
+
+    try{
+        const user_token = req.user
+      
+        
+       
+        res.json({
+            message: "INSERT OwnerShipHistory complete!"
+        })
+
+    }catch(error){
+        console.log("can INSERT ownership history")
+        res.status(403).json({
+            message: "authentication fail",
+            error
+        })
+    }
+
+};
+
 
 exports.getMyCollection = async (req, res) => {
 
     try{
-        
         const user_token = req.user
-        console.log("user_token: ", user_token) // ใช้ user_token.idได้
-
-        console.log("user_token.id: ", user_token.id)
-
+      
         const user = await req.db.query("SELECT * FROM user WHERE id = ?", user_token.id)
         const user_id = user[0][0].id
 
-        console.log("user_id: ", user_id)
 
         const product = await req.db.query("SELECT * FROM product WHERE current_owner_id = ? AND status = 'With Owner' AND type = 'Real'", user_id)
-        console.log("product: ", product[0])
 
         res.json({
             message: "SELECT complete!",
@@ -120,14 +171,21 @@ exports.getMyCollection = async (req, res) => {
 
 exports.buyCollection = async (req, res) => {
 
-    const id = req.params.id
+    const { id } = req.params;
     try{
         const user_token = req.user
-        console.log("user_token: ", user_token) // ใช้ user_token.idได้
+
+        const product = await req.db.query("SELECT * FROM product WHERE id = ?", id)
+        const token_id = product[0][0].token_id
+        console.log("token_id Ownership: ", token_id)
+        const from_id = product[0][0].current_owner_id
+
+        const ownershipHistory = await req.db.query("INSERT INTO ownership_history (product_id, token_id, from_user_id, to_user_id) VALUES (?, ?, ?, ?)", [id, token_id, from_id, user_token.id])
         
         const result = await req.db.query("UPDATE product SET current_owner_id = ?, status = 'With Owner' WHERE id = ? AND type='Real'", [user_token.id, id])
 
-
+        
+        
         res.json({
             message: "Buy Product complete!",
             result
@@ -145,8 +203,9 @@ exports.buyCollection = async (req, res) => {
 
 exports.sellCollection = async (req, res) => {
     let id = req.params.id
+    const user_token = req.user
     try{
-        const result = await req.db.query("UPDATE product SET status = 'Sold' WHERE id = ? AND current_owner_id =  ", id)
+        const result = await req.db.query("UPDATE product SET status = 'Sold' WHERE id = ? AND current_owner_id = ? ", [id, user_token.id])
         res.json({
             message: "Sell Collection complete!"
         })
@@ -182,15 +241,11 @@ exports.market = async (req, res) => {
 exports.getMyAsset = async (req, res) => {
 
     try{
-        
         const user_token = req.user
-        console.log("user_token: ", user_token) // ใช้ user_token.idได้
 
         const user_id = user_token.id
-        console.log("user_id: ", user_id)
 
         const product = await req.db.query("SELECT * FROM product WHERE current_owner_id = ? AND status = 'With Owner' AND type = 'Asset'", user_id)
-        console.log("product: ", product[0])
 
         res.json({
             message: "SELECT complete!",
@@ -227,20 +282,19 @@ exports.sellAsset = async (req, res) => {
 };
 
 exports.trading = async (req, res) => {
-    try{
-        const product = await req.db.query("SELECT product.*, COALESCE(user.firstname, 'Manufacturer') AS owner_name FROM product LEFT JOIN user ON product.current_owner_id = user.id WHERE status IN ('Sold', 'In Custody') AND type = 'Asset'")
-
+    try {
+        const query = ""
+            
+        ;
+        const [products] = await req.db.query("SELECT p.*, u.email AS owner_email FROM product p LEFT JOIN user u ON p.current_owner_id = u.id WHERE p.status IN ('Sold', 'In Custody') AND p.type = 'Asset' ");
         res.json({
             message: "SELECT complete!",
-            data: product[0]
-        })
+            data: products
+        });
 
-    }catch(error){
-        console.log("can not get asset")
-        res.status(403).json({
-            message: "authentication fail",
-            error
-        })
+    } catch (error) {
+        console.error("Error in trading:", error);
+        res.status(500).json({ message: "Something went wrong" });
     }
 };
 
@@ -252,12 +306,17 @@ exports.buyAsset = async (req, res) => {
         console.log("user_token: ", user_token) // ใช้ user_token.idได้
 
         const product = await req.db.query("SELECT * FROM product WHERE id = ? AND type='Asset'", id)
+        // id ของเจ้าของเก่า
         const oldOwnerId = product[0][0].current_owner_id
+        // wallet address ของเจ้าของเก่า --- oldOwnerWallet[0][0].wallet_address
+        const oldOwnerWallet = await req.db.query("SELECT wallet_address FROM user WHERE id = ?", oldOwnerId)
+        // wallet address ของเจ้าของใหม่ --- newOwnerWallet[0][0].wallet_address
+        const newOwnerWallet = await req.db.query("SELECT wallet_address FROM user WHERE id = ?", user_token.id)
         console.log("oldOwnerId: ", oldOwnerId)
-        
-        const result = await req.db.query("UPDATE product SET current_owner_id = ?, status = 'With Owner' WHERE id = ? AND type='Asset'", [user_token.id, id])
+        console.log("oldOwnerWallet: ", oldOwnerWallet[0][0].wallet_address)
+        console.log("newOwnerWallet: ", newOwnerWallet[0][0].wallet_address)
 
-        // const history = await req.db.query("INSERT INTO `ownership_history`(`product_id`, `from_user_id`, `to_user_id`) VALUES (?, ?, ?, ?)", [id, oldOwnerId, user_token.id])
+        const result = await req.db.query("UPDATE product SET current_owner_id = ?, status = 'With Owner' WHERE id = ? AND type='Asset'", [user_token.id, id])
 
         res.json({
             message: "Buy Product complete!",
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -5,6 +5,7 @@ const { verifyToken } = require("../middleware/auth.middleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 
 router.patch("/register",verifyToken, authorizeRoles("admin"), productController.registerProductToUser);//register product 
+router.post("/getwallet",verifyToken, authorizeRoles("admin"), productController.getWalletAddress);//get wallet address
 router.post("/add",verifyToken, authorizeRoles("admin"), productController.addProduct); //add product
 router.get("/get",verifyToken, authorizeRoles("admin"), productController.getAllProducts);  // get all products
 
@@ -15,7 +16,7 @@ router.patch("/:id/buycollection", verifyToken, authorizeRoles("user"), productC
 
 router.get("/asset", verifyToken, authorizeRoles("user"), productController.getMyAsset); //get my asset  
 router.patch("/:id/sellasset", verifyToken, authorizeRoles("user"), productController.sellAsset); // sell asset 
-router.get("/trading", productController.trading); // get all assets for sale  
+router.get("/trading", verifyToken, authorizeRoles("user"), productController.trading); // get all assets for sale  
 router.patch("/:id/buyasset", verifyToken, authorizeRoles("user"), productController.buyAsset);//buy asset 
 
 router.get("/:serial/verify", productController.verify);//verify product
